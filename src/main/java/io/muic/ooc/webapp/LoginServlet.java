ecc34bf2fbb577cc2fa4d07ed889d01f83bcfee7
Manit Gandhi <yourgitemail@example.com>
2021-06-27 14:07:19 +0700

    -Add doPost and set error message

--- src/main/java/io/muic/ooc/webapp/LoginServlet.java
+++ src/main/java/io/muic/ooc/webapp/LoginServlet.java
@@ -1,9 +1,11 @@
 package io.muic.ooc.webapp;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -14,9 +16,29 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter()
-                .append("This is admin servlet. It is requested with GET method.")
-                .close();
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/login.jsp");
+        requestDispatcher.include(request,response);
     }
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String error = "";
+
+        //check if user is a valid user
+
+        if(username != null && username.equals("tripleking") && password != null && password.equals("123")){
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+
+            response.sendRedirect("/");
+        }
+        else{
+             error = "Username or password incorrect. Please try again.";
+             request.setAttribute("error", error);
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/login.jsp");
+            requestDispatcher.include(request,response);
+        }
+    }
 }
