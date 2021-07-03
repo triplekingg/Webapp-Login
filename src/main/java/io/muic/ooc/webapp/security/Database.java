849c7139d4d5c71721d5b60cbf8b9e9c050b5684
Manit Gandhi <yourgitemail@example.com>
2021-07-03 14:51:57 +0700

    -Create a function to input values from database into a hashmap and return it

--- src/main/java/io/muic/ooc/webapp/security/Database.java
+++ src/main/java/io/muic/ooc/webapp/security/Database.java
@@ -1,11 +1,12 @@
 package io.muic.ooc.webapp.security;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Database {
+
+
     public Connection connect(){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -16,23 +17,43 @@ public class Database {
         catch(Exception e){ return null;}
     }
 
+//    public boolean ifExists(String user) throws SQLException {
+//        String sql = "SELECT * FROM User_List as u WHERE u.user_id ='"+user+"'";
+//        Connection con = connect();
+//        Statement stmt=con.createStatement();
+//        ResultSet rs=stmt.executeQuery(sql);
+//        return rs.next();
+//    }
 
-    public static void main(String[] args) {
-        try{
-            Database db = new Database();
-            Connection con = db.connect();
-            Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery("SELECT * FROM User_List as u WHERE u.user_id = 'tpk'");
-            if(rs.next()==false){
-                System.out.println("false");
-            }
-            else{
-                System.out.println("True");
-            }
-            while(rs.next())
-                System.out.println(rs.getInt(1)+"  "+rs.getString(3)+"  "+rs.getString(4));
-            con.close();
-        }catch(Exception e){ System.out.println(e);}
+    public Map<String,User> returnHashmap(Map<String,User> users) throws SQLException {
+        String sql = "select * from User_List";
+        Connection con = connect();
+        Statement stmt=con.createStatement();
+        ResultSet rs=stmt.executeQuery(sql);
+        while(rs.next()){
+            users.put(rs.getString(1), new User(rs.getString(1),rs.getString(2)));
+        }
+        return  users;
     }
+
+
+//    public static void main(String[] args) {
+//        try{
+//            String u = "t";
+//            String sql = "SELECT * FROM User_List as u WHERE u.user_id ='"+u+"'";
+//            Connection con = connect();
+//            Statement stmt=con.createStatement();
+//            ResultSet rs=stmt.executeQuery(sql);
+//            if(rs.next()==false){
+//                System.out.println("false");
+//            }
+//            else{
+//                System.out.println("True");
+//            }
+//            while(rs.next())
+//                System.out.println(rs.getInt(1)+"  "+rs.getString(3)+"  "+rs.getString(4));
+//            con.close();
+//        }catch(Exception e){ System.out.println(e);}
+//    }
 }
 
