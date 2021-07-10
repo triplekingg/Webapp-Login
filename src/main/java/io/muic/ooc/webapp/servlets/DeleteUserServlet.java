fd33a0cd5465ba2bd56361168d9f7452b6a2f647
Manit Gandhi <yourgitemail@example.com>
2021-07-10 15:08:51 +0700

    Prevent deleting account from server side

--- src/main/java/io/muic/ooc/webapp/servlets/DeleteUserServlet.java
+++ src/main/java/io/muic/ooc/webapp/servlets/DeleteUserServlet.java
@@ -1,5 +1,6 @@
 package io.muic.ooc.webapp.servlets;
 
+import com.mysql.cj.util.StringUtils;
 import io.muic.ooc.webapp.security.User;
 
 import javax.servlet.RequestDispatcher;
@@ -21,14 +22,21 @@ public class DeleteUserServlet extends AbstractRoutableHttpServlet{
             try{
                 User currentUser = userService.findByUsername(username);
                 User deleteUser = userService.findByUsername(request.getParameter("username"));
-                if(userService.deleteUser(deleteUser.getUsername())){
-                    request.setAttribute("hasError",false);
-                    request.getSession().setAttribute("message",String.format("Successfully deleted User %s.", deleteUser.getUsername()));
+                if(currentUser.getUsername().equals(deleteUser.getUsername())){
+                    request.getSession().setAttribute("hasError", true);
+                    request.getSession().setAttribute("message", "You are not allowed to delete your own account.");
                 }
                 else{
-                    request.setAttribute("hasError",true);
-                    request.getSession().setAttribute("message",String.format("Unsuccessful deletion of User %s.", request.getParameter("username")));
+                    if(userService.deleteUser(deleteUser.getUsername())){
+                        request.setAttribute("hasError",false);
+                        request.getSession().setAttribute("message",String.format("Successfully deleted User %s.", deleteUser.getUsername()));
+                    }
+                    else{
+                        request.setAttribute("hasError",true);
+                        request.getSession().setAttribute("message",String.format("Unsuccessful deletion of User %s.", request.getParameter("username")));
+                    }
                 }
+
             }
             catch (Exception e){
                 request.setAttribute("hasError",true);
